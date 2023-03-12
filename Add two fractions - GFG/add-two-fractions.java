2191723f3c5c0146637e5c30a67899a9cb6b3964
sunidhi207 <114490520+sunidhi207@users.noreply.github.com>
2023-03-12 21:38:08 +0530

    Added solution - LeetHub

---
+++ Add two fractions - GFG/add-two-fractions.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Fraction_Addition
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			int d = sc.nextInt();
+			
+			GfG g = new GfG();
+			g.addFraction(a,b,c,d);
+		T--;
+		}
+	}
+}
+// } Driver Code Ends
+
+class GfG
+{ 	
+    void addFraction(int num1, int den1, int num2, int den2)
+    {
+        int num_sum=num1*den2+num2*den1;
+        int den_sum=den1*den2;
+        int i;
+     for(i=2;i<=99;i++)
+            {
+                if(num_sum%i==0 && den_sum%i==0)
+                {
+                num_sum=num_sum/i;
+                den_sum=den_sum/i;
+                i--;
+                }
+            }
+            System.out.println(num_sum+"/"+den_sum);
+        
+    }
+}
